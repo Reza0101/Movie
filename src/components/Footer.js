636a7b34e5bb9556d5c95bdbb8cfe80b189230e9
Reza <121669057+Reza0101@users.noreply.github.com>
2024-05-12 17:22:23 +0330

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
 
                 <AiOutlineHeart className="text-red-500 text-[22px]" />
 
-                <p>By Fateme Ghafari <a className="text-red-500" href="https://fatemeweb.vercel.app/en">WEBSITE</a> And Developt By Reza Siahi</p>
+                <p>By <a className="text-red-500" href="https://fatemeweb.vercel.app/en">Fateme Ghafari</a> And Developt By Reza Siahi</p>
             </div>
         </div>
     );
