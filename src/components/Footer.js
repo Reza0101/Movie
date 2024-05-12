0c1f0c1034e49f8c0a573f6bd39aff3fe3a712de
Reza <121669057+Reza0101@users.noreply.github.com>
2024-05-12 17:53:29 +0330

    Change target link footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
             <p className="w-[100%] h-[1px] bg-red-500"></p>
             <div className="text-center p-5 text-white flex items-center gap-1 justify-center">
                 <p>Designed with</p>
-
                 <AiOutlineHeart className="text-red-500 text-[22px]" />
 
                 <p className="text-sm md:text-md text-center"
-    >By <a className="text-red-500 font-bold" href="https://fatemeweb.vercel.app/en">Fateme Ghafari</a> And Developt By Reza Siahi</p>
+    >By <a className="text-red-500 font-bold" target="_blank" 
+                rel="noreferrer"  href="https://fatemeweb.vercel.app/en">Fateme Ghafari</a> And Developt By Reza Siahi</p>
             </div>
         </div>
     );
